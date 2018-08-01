22ad0da197433eca6e535042730e67dbed5f8c3e
ONE <ONE@DESKTOP-1PTPJ7M>
2018-07-31 19:45:12 -0700

    Initial Commit

--- src/latihan/Latihan1.java
+++ src/latihan/Latihan1.java
@@ -30,7 +30,7 @@ public class Latihan1 {
        System.out.println("nIF.. 3");
        if(a > 5) {
            System.out.println("isi variabel a : "+ a);
-           System.out.println("Nilai a lebih dari 5");
+           System.out.println("Nilai a lebih dari 5");                
        } 
    }
     
